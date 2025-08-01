b950328e00c6fdf07f04c76a4d2baf03a9a3d916
Nigel0412 <chua.avrilnigel0412@gmail.com>
2025-08-02 05:00:36 +0800

    Overthinker is me, I am in the spectrum wiiihh.

--- autoencoder.py
+++ autoencoder.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from keras.models import Model
 from keras.layers import Input, Conv2D, BatchNormalization, LeakyReLU, Flatten, Dense, Reshape, Conv2DTranspose, ReLU, Activation, Lambda, Layer
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import MeanSquaredError
+from tensorflow.keras.losses import MeanSquaredError, Loss
 from tensorflow.keras import backend as K
 import numpy as np
 import keras
@@ -33,16 +33,11 @@ class Autoencoder(ABC):
         self.decoder.summary()
         self.model.summary()
     
+    @abstractmethod
     def compile(self, learning_rate=0.0001):
         """Compiles the autoencoder model with the specified optimizer and loss function."""
         if self.model is None:
             raise ValueError("Model has not been built yet. Call build_model() first.")
-        
-        self.model.compile(
-            optimizer=Adam(learning_rate=learning_rate),
-            loss=MeanSquaredError(),
-            metrics=['mae', 'mse']  # both Keras shorthand
-        )
         
     def train(self, **kwargs):
         train_config = kwargs.get('train_config', {})
@@ -74,11 +69,12 @@ class Autoencoder(ABC):
         # self._build_encoder()
         # self._build_decoder()
         self._build_autoencoder()
-        
+    
+    @abstractmethod
     def _build_autoencoder(self):
         encoder_output = self._build_encoder()
         decoder_output = self._build_decoder(encoder_output)
-        self.model = Model(inputs=self._model_input, outputs=decoder_output, name='autoencoder')
+        return decoder_output
     
     def _build_decoder(self, encoder_output):
         # decoder_input = self._add_decoder_input_layer()
@@ -187,7 +183,9 @@ class Autoencoder(ABC):
     
     @abstractmethod
     def _add_bottleneck_layer(self, x):
-        pass  # This method should be implemented in subclasses to define the bottleneck layer of the encoder.
+        self._shape_before_bottleneck = K.int_shape(x)[1:] # Saves the shape of the feature maps before the bottleneck layer.
+        x = Flatten(name='encoder_flatten_layer')(x) # Flattens the feature maps into a vector.
+        return x  # This is an abstract method to be implemented by subclasses.
 
 class VanillaAutoencoder(Autoencoder):
     def __init__(self, input_shape, latent_space_dim, decoder_out_filter, **kwargs):
@@ -196,10 +194,21 @@ class VanillaAutoencoder(Autoencoder):
         self.build_model()
     
     def _add_bottleneck_layer(self, x):
-        self._shape_before_bottleneck = K.int_shape(x)[1:] # Saves the shape of the feature maps before the bottleneck layer.
-        x = Flatten(name='encoder_flatten_layer')(x) # Flattens the feature maps into a vector.
+        x = super()._add_bottleneck_layer(x)  # Call the parent method to flatten the feature maps.
         x = Dense(self.latent_space_dim, name='encoder_bottleneck_layer')(x) # Dense layer to create the bottleneck, reducing the dimensionality to the latent space.
         return x
+    
+    def _build_autoencoder(self):
+        decoder_output = super()._build_autoencoder()  # Call the parent method to build the decoder.
+        self.model = Model(inputs=self._model_input, outputs=decoder_output, name='vanilla_autoencoder')
+        
+    def compile(self, learning_rate=0.0001):
+        super().compile(learning_rate)  # Call the parent compile method.
+        self.model.compile(
+            optimizer=Adam(learning_rate=learning_rate),
+            loss=MeanSquaredError(),
+            metrics=['mae', 'mse']  # both Keras shorthand
+        )
 
 class VariationalAutoencoder(Autoencoder):
     def __init__(self, input_shape, latent_space_dim, decoder_out_filter, **kwargs):
@@ -208,8 +217,7 @@ class VariationalAutoencoder(Autoencoder):
         self.build_model()
     
     def _add_bottleneck_layer(self, x):
-        self._shape_before_bottleneck = K.int_shape(x)[1:]  # Saves the shape of the feature maps before the bottleneck layer.
-        x = Flatten(name='encoder_flatten_layer')(x)  # Flattens the feature maps into a vector.
+        x = super()._add_bottleneck_layer(x)  # Call the parent method to flatten the feature maps.
         
         # Not Sequential, so we need to define the mu and log_variance layers separately.
         # These layers will output the parameters of the latent space distribution.
@@ -227,6 +235,30 @@ class VariationalAutoencoder(Autoencoder):
         
         x = Sampling(name='encoder_output')([self.mu, self.log_variance])  # Sampling layer to sample from the latent space distribution.
         return x
+    
+    def _build_autoencoder(self):
+        decoder_output = super()._build_autoencoder()  # Call the parent method to build the decoder.
+        self.model = Model(inputs=self._model_input, outputs=[decoder_output, self.mu, self.log_variance], name='variational_autoencoder')
+    
+    def compile(self, learning_rate=0.0001):
+        super().compile(learning_rate)  # Call the parent compile method.
+        loss_fn = VAELoss(recon_weight=1000.0, kl_weight=1.0)  # Custom VAE loss function.
+        # The loss function will compute the reconstruction loss and KL divergence.
+        self.model.compile(
+            optimizer=Adam(learning_rate=learning_rate),
+            loss=loss_fn,  # Custom VAE loss function.
+            metrics=[
+                tf.keras.metrics.MeanMetricWrapper(
+                    fn=lambda y_true, y_pred: loss_fn.reconstruction_loss(y_true, y_pred),
+                    name="reconstruction_loss"
+                ),
+                tf.keras.metrics.MeanMetricWrapper(
+                    fn=lambda y_true, y_pred: loss_fn.kl_loss(y_pred),
+                    name="kl_divergence_loss"
+                )
+            ]  # Custom metrics for monitoring.
+        )
+
 
 class Sampling(Layer):
     @tf.function
@@ -236,15 +268,63 @@ class Sampling(Layer):
         sampled_point =  mu + tf.exp(log_variance / 2) * epsilon  # Reparameterization trick: mu + sigma * epsilon, where sigma = exp(log_variance / 2).
         # This allows gradients to flow through the sampling process. 
         return sampled_point
+
+class VAELoss(tf.keras.losses.Loss):
+    def __init__(self, recon_weight=1000.0, kl_weight=1.0, name="vae_loss"):
+        super().__init__(name=name)
+        self.recon_weight = recon_weight
+        self.kl_weight = kl_weight
+
+    def call(self, y_true, y_pred):
+        y_recon, mu, log_variance = y_pred
+
+        reconstruction_loss = self._calculate_reconstruction_loss(y_true, y_recon)
+        kl_divergence_loss = self._calculate_Kullback_Leibler_divergence(log_variance, mu)
+        
+        # Add metrics for monitoring
+        self.add_metric(tf.reduce_mean(reconstruction_loss), name="reconstruction_loss", aggregation="mean")
+        self.add_metric(tf.reduce_mean(kl_divergence_loss), name="kl_loss", aggregation="mean")
+        # [0.02, 0.03, 0.01, 0.04]
+        # tf.reduce_mean(reconstruction_loss)  # = 0.025
+        # self.add_metric(reconstruction_loss, name="reconstruction_loss") throws an error
+
+        total_loss = self.recon_weight * reconstruction_loss + self.kl_weight * kl_divergence_loss
+        return total_loss
+
+    def _calculate_reconstruction_loss(self, y_true, y_recon):
+        """Calculates the reconstruction loss (mean squared error per sample)."""
+        error = y_true - y_recon
+        reconstruction_loss = tf.reduce_mean(tf.square(error), axis=[1, 2, 3])  # shape: (batch_size,)
+        return reconstruction_loss
+
+    def _calculate_Kullback_Leibler_divergence(self, log_variance, mu):
+        """Calculates the KL divergence loss per sample."""
+        kl_divergence_loss = -0.5 * tf.reduce_sum(
+            1 + log_variance - tf.square(mu) - tf.exp(log_variance), axis=1
+        )  # shape: (batch_size,)
+        return kl_divergence_loss
     
+    def reconstruction_loss(self, y_true, y_pred):
+        y_recon, _, _ = y_pred
+        per_sample_loss = self._calculate_reconstruction_loss(y_true, y_recon)  # shape: (batch_size,)
+        return tf.reduce_mean(per_sample_loss)
+
+    def kl_loss(self, y_pred):
+        _, mu, log_variance = y_pred
+        per_sample_loss = self._calculate_Kullback_Leibler_divergence(log_variance, mu)  # shape: (batch_size,)
+        return tf.reduce_mean(per_sample_loss)
+
+
 if __name__ == "__main__":
     input_shape = (64, 64, 3)  # Example input shape
     latent_space_dim = 128  # Example latent space dimension
     decoder_out_filter = 3  # Example output filter for the decoder (e.g., 3 for RGB images)
-    autoencoder = VanillaAutoencoder(input_shape, latent_space_dim, decoder_out_filter, conv_layers_config=[
+    autoencoder = VariationalAutoencoder(input_shape, latent_space_dim, decoder_out_filter, conv_layers_config=[
         {'filters': 32, 'kernel_size': (3, 3), 'strides': (2, 2)},
         {'filters': 64, 'kernel_size': (3, 3), 'strides': (2, 2)},
         {'filters': 128, 'kernel_size': (3, 3), 'strides': (2, 2)}
     ])
     autoencoder.summary()
     print("Encoder model built successfully.")
+    autoencoder.compile(learning_rate=0.001)
+    print("Autoencoder compiled successfully.")
